cea96b918c2df2c60d1985b95a36c98646d6bc72
PETERSBURG\Leonid_Borisevich <leonid_borisevich@epam.com>
2018-06-13 23:03:14 +0300

    init

---
+++ src/test/java/speeeedy/practice/AppTest.java
@@ -0,0 +1,16 @@
+package speeeedy.practice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void dumbTest() {
+        Assert.assertThat(true, is(true));
+    }
+}
